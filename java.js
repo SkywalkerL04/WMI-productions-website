490a01ef546c1c70d64db509b6d6bfdc9b2b4d88
SkywalkerL04 <160527909+SkywalkerL04@users.noreply.github.com>
2024-05-14 19:28:13 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.menu-items').classList.toggle('show');
+});
+
